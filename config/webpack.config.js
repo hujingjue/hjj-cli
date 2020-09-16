18d8a7ee1ea00b3ebec61085b05f277164a5659a
jingjue.hu <jingjue.hu@11bee.com>
2020-09-16 11:33:22 +0800

    webpack配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,6 +2,7 @@ const path=require('path')
 const HtmlWebpackPlugin=require('html-webpack-plugin')
 const {CleanWebpackPlugin}=require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 
 module.exports={
     entry:{
@@ -16,6 +17,20 @@ module.exports={
             {
                 test:/\.css|s[ac]ss$/,
                 use:[MiniCssExtractPlugin.loader,'css-loader','sass-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:[
+                    {
+                        loader:'babel-loader',
+                        options: {
+                            plugins: ['lodash'],//只加载lodash用到的模块
+                            presets: [['env', { 'modules': false, 'targets': { 'node': 4 } }]]
+                        }
+                    }
+                ]
+                
             }
         ]
     },
@@ -39,5 +54,6 @@ module.exports={
         new MiniCssExtractPlugin({
             filename:'style.css'
         }),
+        new LodashModuleReplacementPlugin
     ]
 }
